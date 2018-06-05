66d3d7cc2efcaca2f99042a4e841b68b6b903735
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2018-06-05 12:55:21 +0300

    fix linting issues

--- src/App.js
+++ src/App.js
@@ -1,75 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap/dist/js/bootstrap'
-import FormContainer from './Components/Form';
-import NumberList from './Components/List';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap';
 import Navbar from './Components/Navbar';
-import WeatherCard from './Components/CustomComponents/WeatherCard'
+import WeatherCard from './Components/CustomComponents/WeatherCard';
 
 class App extends Component {
-  WEATHER_BASE_URL="http://api.openweathermap.org/data/2.5/weather?q="
-  appid="68bceb4fa74247f891b72a68a008b413"
-  ICON_BASE_URL="http://openweathermap.org/img/w/"
-  constructor(props){
+  WEATHER_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  appid = '68bceb4fa74247f891b72a68a008b413';
+  ICON_BASE_URL = 'http://openweathermap.org/img/w/';
+  constructor(props) {
     super(props);
-    this.state={
-      city:'',
-      data: {}
-    },
-    this.handleSubmit=this.handleSubmit.bind(this);
+    this.state = {
+      city: '',
+      data: {},
+    }
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   getWeatherFromApiAsync(city) {
-    return fetch(this.WEATHER_BASE_URL+this.state.city+"&appid="+this.appid)
-      .then((response) => response.json())
-      .then((responseJson) => {
-         console.log(responseJson);
+    return fetch(
+      this.WEATHER_BASE_URL + this.state.city + '&appid=' + this.appid,
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
         this.setState({
-          data: {"city":responseJson.name,
-          "temp":responseJson.main.temp,
-          "weather":responseJson.weather[0].main,
-          "desc":responseJson.weather[0].description,
-          "iconUrl":this.ICON_BASE_URL+responseJson.weather[0].icon+".png"}
-        })
-      
+          data: {
+            city: responseJson.name,
+            temp: responseJson.main.temp,
+            weather: responseJson.weather[0].main,
+            desc: responseJson.weather[0].description,
+            iconUrl: this.ICON_BASE_URL + responseJson.weather[0].icon + '.png',
+          },
+        });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
   }
 
-  handleSubmit(e){
-    e.preventDefault()
-    this.getWeatherFromApiAsync(this.state.city)
-    console.log(e)
-   }
-    
-   handleChange = ({target:{value}}) => {
-     this.setState({
-       city:value,
-     })
-   }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getWeatherFromApiAsync(this.state.city);
+    console.log(e);
+  }
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      city: value,
+    });
+  };
   render() {
     return (
       <div className="App">
         <header>
-          <Navbar 
+          <Navbar
             handleSubmit={this.handleSubmit}
             handleCityChange={e => this.handleChange(e)}
             city={this.state.city}
-            />
+          />
         </header>
         <div className="container">
           <div className="row">
-             <div className="col-12">
+            <div className="col-12">
               <WeatherCard weatherInfo={this.state.data} />
-             </div>
+            </div>
           </div>
         </div>
-
-         
       </div>
     );
   }
--- src/Components/CustomComponents/WeatherCard.js
+++ src/Components/CustomComponents/WeatherCard.js
@@ -1,18 +1,24 @@
-import React,{Fragment} from 'react'
+import React from 'react';
 
-
-
-const WeatherCard=(props)=>(
-<div className="card" style={{width: "18rem"}}>
-  <div className="card-body">
-    <h5 className="card-title">{props.weatherInfo.city}</h5>
-    <h6 className="card-subtitle mb-2 text-muted"><span><img src={props.weatherInfo.iconUrl}/>  </span>  {props.weatherInfo.desc}</h6>
-    <p className="card-text">{props.weatherInfo.temp}</p>
-    <a href="#" className="card-link">Card link</a>
-    <a href="#" className="card-link">Another link</a>
+const WeatherCard = props => (
+  <div className="card" style={{ width: '18rem' }}>
+    <div className="card-body">
+      <h5 className="card-title">{props.weatherInfo.city}</h5>
+      <h6 className="card-subtitle mb-2 text-muted">
+        <span>
+          <img src={props.weatherInfo.iconUrl} />{' '}
+        </span>{' '}
+        {props.weatherInfo.desc}
+      </h6>
+      <p className="card-text">{props.weatherInfo.temp}</p>
+      <a href="#" className="card-link">
+        Card link
+      </a>
+      <a href="#" className="card-link">
+        Another link
+      </a>
+    </div>
   </div>
-</div>
 );
 
-
-export default WeatherCard
+export default WeatherCard;
